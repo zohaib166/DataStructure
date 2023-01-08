df3c4be07006f2553b150278256a9d2012dad9a6
zohaib166 <zohaib166@gmail.com>
2023-01-08 16:01:53 +0530

    recursion lot 2

--- Recursion/15_FibonacciRecursive.cpp
+++ Recursion/15_FibonacciRecursive.cpp
@@ -7,21 +7,7 @@ int rfib(int n) {
     }
     return rfib(n-2) + rfib(n-1);
 }
-int F[10];
-int mfib(int n) {
-    if(n<=1) {
-        F[n] = n;
-        return n;
-    } else {
-        if(F[n-2]==-1) {
-            F[n-2] = mfib(n-2);
-        } 
-        if(F[n-1]==-1) {
-            F[n-1] = mfib(n-1);
-        }
-        return F[n-1]+F[n-2];
-    }
-}
+
 int main()
 {
     for(int i=0;i<10;i++) {
---
+++ Recursion/16_FibonacciMemoization.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+//0 1 1 2 3 5
+
+int F[10];
+int mfib(int n) {
+    if(n<=1) {
+        F[n] = n;
+        return n;
+    } else {
+        if(F[n-2]==-1) {
+            F[n-2] = mfib(n-2);
+        } 
+        if(F[n-1]==-1) {
+            F[n-1] = mfib(n-1);
+        }
+        return F[n-1]+F[n-2];
+    }
+}
+int main()
+{
+    for(int i=0;i<10;i++) {
+        F[i] = -1;
+    }
+    int n = 5;
+    cout << mfib(n);
+    return 0;
+}
