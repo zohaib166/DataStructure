e8a0be306c2a2ebae78d91068df5be9b126a7baa
[Zohaib Hasan] <[zohaib166@gmail.com]>
2023-01-20 15:32:37 +0530

    CLL create & display

---
+++ CircularLinkedList/CreateDisplay.cpp
@@ -0,0 +1,42 @@
+#include <cstdio>
+#include <cstdlib>
+
+typedef struct _node
+{
+    int data;
+    struct _node *next;
+} Node;
+
+Node *head = NULL;
+
+void create(int A[], int size) {
+    Node *temp, *new_node;
+    head = (Node*) malloc(sizeof(Node));
+    temp = head;
+    temp->data = A[0];
+    temp->next = NULL;
+    for(int i=1; i<size; i++) {
+        new_node = (Node*) malloc(sizeof(Node));
+        temp->next = new_node;
+        new_node->data = A[i];
+        temp = temp->next;
+    }
+    //Link back to head
+    temp->next = head;
+}
+
+void display(Node *t) {
+    do {
+        printf("%d ", t->data);
+        t=t->next;
+    } while(t!=head);
+
+    printf("\n");
+}
+
+int main() {
+    int A[] = {3,4,5,6,7};
+    int size = 5;
+    create(A, size);
+    display(head);
+}
