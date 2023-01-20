a952ae79417d713b6e0d7fa3c7718f2cf904448c
[Zohaib Hasan] <[zohaib166@gmail.com]>
2023-01-20 15:59:04 +0530

    recursive display added

--- CircularLinkedList/CreateDisplay.cpp
+++ CircularLinkedList/CreateDisplay.cpp
@@ -34,9 +34,22 @@ void display(Node *t) {
     printf("\n");
 }
 
+/**static variable 'flag' will check whether pointer 't'
+is visiting head for the second time or not*/
+void display_recur(Node *t) {
+    static int flag = 0;
+    if(t!=head || flag==0) {
+        flag = 1;
+        printf("%d ", t->data);
+        display_recur(t->next);
+    }
+    flag = 1;
+}
+
 int main() {
     int A[] = {3,4,5,6,7};
     int size = 5;
     create(A, size);
-    display(head);
+    //display(head);
+    display_recur(head);
 }
