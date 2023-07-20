e104853ada4a1bcdbfdc38dd0386b6d32be346be
zohaib166 <zohaib166@gmail.com>
2023-07-20 08:01:57 +0530

    polynomial addition

--- LinkedList/LLPolynomial.cpp
+++ LinkedList/LLPolynomial.cpp
@@ -8,36 +8,73 @@ typedef struct _node {
 } Node;
 
 void create_node(Node **p, int c, int e) {
-    Node *temp = NULL;
-    Node *temp1 = *p;
-    if(*p==NULL) {
+    Node *temp;
+    if((*p)==NULL) {
         temp = (Node*) malloc(sizeof(Node));
         temp->coeff = c;
         temp->deg = e;
-        temp->link = NULL;
         *p = temp;
+        temp->link = (Node*) malloc(sizeof(Node));
+        temp = temp->link;
+        temp->link = NULL;
     } else {
-        while(temp1->link){
-            temp1=temp1->link;
-        }
-        temp = (Node*) malloc(sizeof(Node));
         temp->coeff = c;
         temp->deg = e;
+        temp->link = (Node*) malloc(sizeof(Node));
+        temp = temp->link;
         temp->link = NULL;
-        temp1->link = temp;
     }
 }
 
 void display(Node *p) {
-    while(p) {
+    while(p->link) {
         printf("%dx^%d", p->coeff, p->deg);
-        
-        if(p->deg!=0 && p->link!=NULL) {
+        p = p->link;
+        if(p->deg>=0 && p->link!=NULL) {
             printf(" + ");
         }
-        p = p->link;
     }
     printf("\n");
+}
+
+Node* addPoly(Node* p1, Node *p2) {
+    Node *result = (Node*) malloc(sizeof(Node));
+    Node *temp = result;
+    while(p1->link && p2->link) {
+        if(p1->deg > p2->deg) {
+            temp->coeff = p1->coeff;
+            temp->deg = p1->deg;
+            p1=p1->link;
+        } else if(p2->deg > p1->deg){
+            temp->coeff = p2->coeff;
+            temp->deg = p2->deg;
+            p2=p2->link;
+        } else {
+            temp->coeff = p1->coeff+p2->coeff;
+            temp->deg = p1->deg;
+            p1=p1->link;
+            p2=p2->link;
+        }
+        temp->link = (Node*)malloc(sizeof(Node));
+        temp = temp->link;
+        temp->link = NULL;
+    }
+    while(p1->link || p2->link) {
+        if(p1->link) {
+            temp->coeff = p1->coeff;
+            temp->deg = p1->deg;
+            p1 = p1->link;
+        } 
+        else {
+            temp->coeff = p2->coeff;
+            temp->deg = p2->deg;
+            p2 = p2->link;
+        }
+        temp->link = (Node*)malloc(sizeof(Node));
+        temp = temp->link;
+        temp->link = NULL;
+    }
+    return result;
 }
 
 int main() {
@@ -52,4 +89,8 @@ int main() {
     create_node(&p2, 5, 1);
     create_node(&p2, 7, 0);
     display(p2);
+
+    Node *result = addPoly(p1, p2);
+
+    display(result);
 }
