83422b84e441b68db3fe3de8148ce8992dc425e7
zohaib166 <zohaib166@gmail.com>
2024-11-24 14:41:49 +0530

    infix to postfix

--- Stack/InfixtoPostfixWParentheses.cpp
+++ Stack/InfixtoPostfixWParentheses.cpp
@@ -1,107 +1,60 @@
-#include <cstdio>
 #include <iostream>
-#include <cstring>
+#include <stack>
+#include <vector>
+#include <cctype>
+
 using namespace std;
 
-typedef struct _stack {
-    char *data;
-    int size;
-    int top = -1;
-} stack;
-
-void init(stack *s, int size) {
-    s->size = size;
-    s->data = (char*) malloc(sizeof(char)*size);
+int preced(char ch) {
+   if(ch == '+' || ch == '-') {
+      return 1;              //Precedence of + or - is 1
+   }else if(ch == '*' || ch == '/') {
+      return 2;            //Precedence of * or / is 2
+   }else if(ch == '^') {
+      return 3;            //Precedence of ^ is 3
+   }else {
+      return 0;
+   }
 }
 
-void push(stack *s, int n) {
-    s->top++;
-    if(s->top == s->size-1) {
-        cout << "Stack is Full" << endl;
-        return;
-    }
-    s->data[s->top] = n;
-}
-
-char pop(stack *s) {
-    if(s->top==-1) {
-        return -1;
-    }
-
-    return s->data[s->top--];
-}
-
-char top(stack *s) {
-    if(s->top==-1) {
-        return -1;
-    }
-    return s->data[s->top];
-}
-
-int empty(stack *s) {
-    if(s->top==-1) return 1;
-    else return 0;
-}
-
-int getPrecedence(char op)
+int main()
 {
-	switch (op) {
-	case '+':
-	case '-':
-		return 1;
-	case '*':
-	case '/':
-		return 2;
-	case '^':
-		return 3;
-	default: 
-		return -1;
-	}
-}
-
-int isOperator(char ch)
-{
-	return (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '^' || ch=='(' || ch==')');
-}
-
-void infix2postfix(const char *infix, char *postfix) {
-    int i=0,j=0;
-    stack s;
-    int len = strlen(infix);
-    init(&s, len);
-    for (i = 0, j = 0; i < len; i++) {
-        if(!isOperator(infix[i])) {
-            postfix[j++] = infix[i];
-        } else if(infix[i]=='(') {
-            push(&s, infix[i]);
-        } else if(infix[i]==')') {
-            while(!empty(&s) && top(&s)!='(') {
-                postfix[j++] = pop(&s);
-            }
-            if(!empty(&s) && top(&s)!='(') {
-                printf("Invalid Expression\n");
-                return;
+    string infix = "A/(B+(C-D))*E";
+    vector<char> postfix;
+    stack<char> s;
+    for(int i=0;i<infix.length();i++)
+    {
+        char curr = infix[i];
+        if(isalnum(curr))
+        {
+            postfix.push_back(curr);
+        } else {
+            
+            if(curr == '(') {
+                s.push(curr);
+            } else if(curr == ')') {
+                while(!s.empty() && s.top()!='(') {
+                    postfix.push_back(s.top());
+                    s.pop();
+                }
+                s.pop();
             } else {
-                pop(&s);
+                while(!s.empty() && preced(curr) <= preced(s.top())) {
+                    if(curr=='^' && s.top()=='^')
+                        break;
+                    postfix.push_back(s.top());
+                    s.pop();
+                }
+                s.push(curr);
             }
-        } else if(isOperator(infix[i])) {
-            while(!empty(&s) && getPrecedence(infix[i]) <= getPrecedence(top(&s))) {
-                postfix[j++] = pop(&s);
-            } 
-            push(&s, infix[i]);
         }
+        
     }
-
-    while(!empty(&s)) {
-        postfix[j++] = pop(&s);
+    while(!s.empty()) {
+        postfix.push_back(s.top());
+        s.pop();
     }
-    postfix[j] = '\0';
+    
+    for(auto x:postfix) 
+        cout << x << " ";
 }
-
-int main() {
-    const char *infix = "a*(b+c)";
-    char *postfix = new char[strlen(infix)+2];
-    infix2postfix(infix, postfix);
-    printf("%s", postfix);
-}
-
